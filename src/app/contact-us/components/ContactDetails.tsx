56173e6b1393e4bb00b1c4cc2ea74a673d9fbb65
04-AKILESH <akileshnagarajan123@gmail.com>
2026-03-03 13:04:54 +0530

    Phone number

--- src/app/contact-us/components/ContactDetails.tsx
+++ src/app/contact-us/components/ContactDetails.tsx
@@ -24,7 +24,7 @@ interface ContactItem {
 
 const CONTACTS: ContactItem[] = [
     { icon: <MdOutlinePhoneInTalk />, label: 'Phone', value: '+97165351536' },
-    { icon: <MdOutlineWhatsapp />, label: 'Whatsapp', value: '+971568284581' },
+    { icon: <MdOutlineWhatsapp />, label: 'Whatsapp', value: '+971509002987' },
     { icon: <MdOutlineMail />, label: 'Email', value: 'sales@pmfworld.com' },
     { icon: <BsShop />, label: 'Address', value: 'Meydan Grandstand, 6th floor, Meydan Road, Nad Al Sheba, Dubai, U.A.E' },
 ];
